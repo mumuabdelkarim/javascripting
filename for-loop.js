53321ee4cd428227cdcccddbe350e7cdf05429c0
Mumu Abdelkarim <m.tarig605@gmail.com>
2022-12-23 00:14:19 -0500

    adding js files from javascripting

---
+++ for-loop.js
@@ -0,0 +1,18 @@
+let total = 0
+let limit = 10
+for (let i = 0; i < limit; i++) {
+  total = total + i
+
+}
+console.log(total)
+// i total
+// 0 0
+// 1 1
+// 2 3
+// 3 6
+// 4 10
+// 5 15
+// 6 21
+// 7 28
+// 8 36
+// 9 45
